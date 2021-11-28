c6fd9303820ade5c7ef4ff18c770cc9799e8ddaf
Yavor Filipov <yavorfilipov9@gmail.com>
2021-11-28 22:31:56 +0200

    Fix api lint and test

--- llama-bot-api/src/index.ts
+++ llama-bot-api/src/index.ts
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 import express, { Application } from "express";
 import Router from "./routes";
 import { startDb } from "./database/db";
