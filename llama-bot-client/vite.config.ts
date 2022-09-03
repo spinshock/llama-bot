60a760a10a4ae3934f69875cc251e9590f51bc2c
Yavor Filipov <yavorfilipov9@gmail.com>
2022-09-03 09:43:48 +0300

    [skip ci]

--- llama-bot-client/vite.config.ts
+++ llama-bot-client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: "automatic" })],
   server: {
     proxy: {
       "/api": {
