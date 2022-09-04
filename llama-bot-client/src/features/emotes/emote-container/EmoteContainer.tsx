2da84a9d4975ea37f095cf36e0b465034fef8c8e
Yavor Filipov <yavorfilipov9@gmail.com>
2022-09-04 05:05:30 +0300

    Copy to clipboard (#23)
    * Copy to clipboard
    
    * Introduce dependency between deploy jobs
    
    * Fix lint

--- llama-bot-client/src/features/emotes/emote-container/EmoteContainer.tsx
+++ llama-bot-client/src/features/emotes/emote-container/EmoteContainer.tsx
@@ -1,11 +1,17 @@
+import { toast } from "react-toastify";
 import { Emote } from "../models/emote";
 import "./EmoteContainer.scss";
 
 export type EmoteProps = Emote;
 
+const copyToClipboard = async (text: string): Promise<void> => {
+  await navigator.clipboard.writeText(text);
+  toast("Copied to clipboard!");
+};
+
 export const EmoteContainer = ({ url, code, author }: EmoteProps) => {
   return (
-    <div className="emote-container">
+    <div className="emote-container" onClick={() => copyToClipboard(code)}>
       <img src={url} />
       <p>{code}</p>
       <p>{author}</p>
